b19496a64af867ed35cec183b7250e44c61a35d6
dov <benscheinberg@gmail.com>
2024-10-07 15:03:14 -0400

    edited the footer logos and spacing for phones

--- src/components/Landing/Testimonials.tsx
+++ src/components/Landing/Testimonials.tsx
@@ -5,34 +5,34 @@ import { FaTwitter, FaGithub, FaDiscord, FaBook, FaTelegram } from "react-icons/
 
 const partners = [
   {
-    icon: <FaDiscord style={{ fontSize: 40 }} />,
+    icon: <FaDiscord style={{ fontSize: 35 }} />,
     link: "https://discord.gg/y7wM5YSpmm",
   },
   {
-    icon: <FaTwitter style={{ fontSize: 40 }} />,
+    icon: <FaTwitter style={{ fontSize: 35 }} />,
     link: "https://x.com/okay_bet_app",
   },
   {
-    icon: <FaTelegram style={{ fontSize: 40 }} />,
+    icon: <FaTelegram style={{ fontSize: 35 }} />,
     link: "https://t.me/+FnKD76WKUxxjYmQx",
   },
   {
-    icon: <FaGithub style={{ fontSize: 40 }} />,
+    icon: <FaGithub style={{ fontSize: 35 }} />,
     link: "https://github.com/Okay-Bet/okay-bet-contracts",
   },
 ];
 
 const Testimonials = () => {
   return (
-    <section className="py-10 bg-primary text-quaternary">
-      <div className="container mx-auto px-6">
+    <section className="py-5 bg-primary text-quaternary">
+      <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
           <a
             href="https://docs.okaybet.fun/docs/intro" 
             className="text-font transform hover:scale-110 transition-transform flex items-center"
           >
-            <FaBook style={{ fontSize: 40 }} />
-            <span className="ml-2 font-heading font-bold">Docs</span>
+            <FaBook style={{ fontSize: 35 }} />
+            <span className="ml-2 mr-2 font-heading font-bold">Docs</span>
           </a>
           <div className="flex items-center space-x-6">
             {partners.map((partner, index) => (
