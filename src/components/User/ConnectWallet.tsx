0cc416d0a385c869370b2070d438fdf823c54c21
dov <benscheinberg@gmail.com>
2025-01-23 18:26:04 -0500

    switched to base gave up on cross chain for now

--- src/components/User/ConnectWallet.tsx
+++ src/components/User/ConnectWallet.tsx
@@ -4,7 +4,7 @@
 "use client";
 import { ConnectButton } from "thirdweb/react";
 import { createWallet, inAppWallet } from "thirdweb/wallets";
-import { defineChain, optimism, polygon } from "thirdweb/chains";
+import { defineChain, optimism, polygon, base } from "thirdweb/chains";
 import { client } from "../../app/client";
 
 const externalWallets = [
@@ -16,7 +16,7 @@ const inAppWalletOption = inAppWallet({
     options: ["google", "facebook", "apple", "phone", "email"],
   },
   smartAccount: {
-    chain: optimism, 
+    chain: base, 
     sponsorGas: true,
   },
 });
@@ -27,7 +27,7 @@ const ConnectWallet = () => {
       <ConnectButton
         client={client}
         wallets={[...externalWallets, inAppWalletOption]}
-        chain={defineChain(optimism)}
+        chain={defineChain(base)}
         theme={"dark"}
         connectModal={{ size: "wide" }}
         appMetadata={{
